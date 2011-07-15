5f28b64a154ee2c2526cfa9eb78f76f36b363e0b
Matthew Ball <mathew.ball@gmail.com>
2011-07-15 18:40:34 +1000

    Initial commit.

---
+++ emacs-dir/abbrev_defs.el
@@ -0,0 +1,97 @@
+;;-*-coding: emacs-mule;-*-
+(define-abbrev-table 'Buffer-menu-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'Custom-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'apropos-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'browse-kill-ring-edit-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'browse-kill-ring-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'calendar-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'comint-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'completion-list-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'conf-colon-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'conf-javaprop-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'conf-ppd-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'conf-space-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'conf-unix-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'conf-windows-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'conf-xdefaults-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'diary-fancy-display-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'diary-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'diff-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'doctex-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'eshell-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'fundamental-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'global-abbrev-table '(    ))
+
+(define-abbrev-table 'gnus-article-edit-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'gnus-sticky-article-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'haskell-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'html-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'latex-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'lisp-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'literate-haskell-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'mail-abbrevs '(    ))
+
+(define-abbrev-table 'message-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'newsticker-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'newsticker-treeview-list-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'org-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'outline-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'plain-tex-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'sgml-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'sh-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'shell-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'slitex-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'snippet-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'special-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'tex-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'tex-shell-abbrev-table '(    ))
+
+(define-abbrev-table 'text-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'vc-hg-incoming-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'vc-hg-log-view-mode-abbrev-table '(    ))
+
+(define-abbrev-table 'vc-hg-outgoing-mode-abbrev-table '(    ))
+
