431412fec25a65a39e6ca6590ff8dd3ba8f081ec
Matthew Ball <mathew.ball@gmail.com>
2013-05-10 07:43:00 +1000

    Updated.
    - Still cleaning .conf-scripts/ config.

--- backup-script.sh
+++
@@ -1,3 +0,0 @@
-## FILE: /home/chu/.conf-scripts/backup-script.sh
-## AUTHOR: Matthew Ball (copyleft 2012)
-
--- restore-script.sh
+++
@@ -1,2 +0,0 @@
-## FILE: /home/chu/.conf-scripts/restore-script.sh
-## AUTHOR: Matthew Ball (copyleft 2012)
